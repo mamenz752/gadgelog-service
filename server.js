2773c3b28c66af2ad7daa4edab0e1a06874bdb50
mamenz752 <mamenz752@gmail.com>
2024-11-26 14:51:21 +0900

    feat: サーバーとDBのコネクション確立

--- server.js
+++ server.js
@@ -3,22 +3,38 @@ const express = require('express');
 const app = express();
 const SERVER_PORT = 5000;
 
+// use middle-ware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // use MongoDB as database
 const mongoose = require('mongoose');
-mongoose.connect(
-  'mongodb://localhost:27017/comments'
-)
 
-const db = mongoose.connection;
+// use Schema and Model
+const Comment = require('./models/comment');
 
-db.once('open', () => {
-  console.log('database connected');
-});
-
-app.get('/', (req, res) => {
-  console.log('hello express');
-});
-
-app.listen(SERVER_PORT, () => {
-  console.log(`server is running on port ${SERVER_PORT}`)
+// MongoDB接続をアプリケーションの起動時に一度だけ行う
+mongoose.connect('mongodb://root:password@localhost:27017/comment?authSource=admin', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
 })
+  .then(() => {
+    console.log('MongoDBに接続しました');
+  })
+  .catch((err) => {
+    console.error('MongoDB接続エラー:', err);
+  });
+
+// POSTリクエストを処理
+
+// get
+// app.get('api/comments/:id', async (req, res) => {
+
+// })
+
+// cors
+
+// use server
+app.listen(SERVER_PORT, () => {
+  console.log(`server is running on port ${SERVER_PORT}`);
+});
