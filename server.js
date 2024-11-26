5c9dbc3a1e207a44069d296bf6ea85a5aea90e76
mamenz752 <mamenz752@gmail.com>
2024-11-26 14:52:17 +0900

    feat: コメント投稿機能の実装

--- server.js
+++ server.js
@@ -26,6 +26,24 @@ mongoose.connect('mongodb://root:password@localhost:27017/comment?authSource=adm
   });
 
 // POSTリクエストを処理
+app.post('/api/comments', async (req, res) => {
+    try {
+      const comment = new Comment({
+        name: req.body.name,
+        comment: req.body.comment,
+        date: new Date(),
+        article_id: req.body.article_id
+      });
+  
+      const result = await comment.save();
+      console.log("コメントの保存成功", result);
+      
+      res.status(201).json({ message: 'Comment saved successfully', data: result });
+    } catch (err) {
+      console.log('エラー:', err);
+      res.status(500).json({ message: 'Server error', error: err });
+    }
+  });
 
 // get
 // app.get('api/comments/:id', async (req, res) => {
